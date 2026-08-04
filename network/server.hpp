0a62e2bd6d4817f52d37bf2f802316e3b3a17ef3
Laurent Brusa <wrmultitudes@gmail.com>
2026-08-04 13:22:20 +0200

    start server

--- network/server.hpp
+++ network/server.hpp
@@ -1,3 +1,21 @@
 #pragma once
 
-class Server {};
+#include "network.hpp"
+#include <cstdlib>
+
+class Server {
+private:
+  std::map<Message::Type, std::function<void(const Message &msg)>> _actions;
+
+public:
+  Server();
+  ~Server();
+  void start(const size_t &p_port);
+  void defineAction(const Message::Type &messageType,
+                    const std::function<void(long long &clientID,
+                                             const Message &msg)> &action);
+  void sendTo(const Message &message, long long clientID);
+  void sendToArray(const Message &message, std::vector<long long> clientIDs);
+  void sendToAll(const Message &message);
+  void update();
+};
